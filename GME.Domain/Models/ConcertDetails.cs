538dc7a4ce1eb8a02d2b35b683c42532e2d0fa20
Jakob Halling <bitbucket@jakobhalling.com>
2019-06-05 21:11:57 +0200

    Boilerplate files added.

---
+++ GME.Domain/Models/ConcertDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace GME.Domain.Models
+{
+    public class ConcertDetails
+    {
+        public string Name { get; set; }
+        public string Venue { get; set; }
+        public string Price { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ GME.Worker/Program.cs
@@ -0,0 +1,42 @@
+﻿using AngleSharp;
+using AngleSharp.Html.Dom;
+using GME.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GME.Worker
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://gaffa.dk/live";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+            var selector = "div.details";
+            var concerts = document.QuerySelectorAll(selector);
+
+            var concertDetails = new List<ConcertDetails>();
+
+            foreach(var concert in concerts)
+            {
+                var anchors = concert.QuerySelectorAll("h3 a").OfType<IHtmlAnchorElement>();
+
+                var details = new ConcertDetails() {
+                    Name = anchors.First().TextContent,
+                    Url = anchors.First().Href,
+                };
+
+                Console.WriteLine($"Concert {details.Name} with url {details.Url} found.");
+                concertDetails.Add(details);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
